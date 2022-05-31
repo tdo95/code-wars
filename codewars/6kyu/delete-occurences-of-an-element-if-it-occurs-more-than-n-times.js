77861b914cbfd892aa2ad27efb76982eea5504a4
Tee O <tdopress@gmail.com>
2022-05-31 10:42:54 -0400

    new solution

---
+++ codewars/6kyu/delete-occurences-of-an-element-if-it-occurs-more-than-n-times.js
@@ -0,0 +1,26 @@
+function deleteNth(arr,n){
+  
+    //pull distinct numbers from array
+    let distinctNums = [];
+    arr.forEach(num => {
+      if( !distinctNums.includes(num) ) {
+        distinctNums.push(num)
+      }
+    });
+   
+    //for loop for each distinct number
+    nextNum: for(let i = 0; i < distinctNums.length; i++){
+      //remove duplicates 
+      let duplicates = 0;
+      for(let j = 0; j < arr.length; j++){
+        if(arr[j] === distinctNums[i]) {
+          duplicates++;
+          if(duplicates > n) {
+            arr = arr.filter((x,ind) => {if(x === distinctNums[i] && ind >= j ) return false;
+                                         else return true;});
+            continue nextNum;
+          }
+        }
+      }
+    }
+    return arr;
