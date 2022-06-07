f3f8c085e142b06dbe3dee9d08d1be93a74d4e91
Tee O <tdopress@gmail.com>
2022-06-07 13:06:25 -0400

    new solution

---
+++ codewars/5kyu/direction-reduction.js
@@ -0,0 +1,14 @@
+function dirReduc(arr){
+  
+    for (let i = 0; i < arr.length; i++) {
+      if( arr[i] === 'NORTH' && arr[i + 1] === 'SOUTH' ||
+          arr[i] === 'SOUTH' && arr[i + 1] === 'NORTH' ||
+          arr[i] === 'WEST' && arr[i + 1] === 'EAST' ||
+          arr[i] === 'EAST' && arr[i + 1] === 'WEST'){
+        arr.splice(i,2);
+        i = i === 0 ? --i : i -= 2;
+      }
+    }
+    
+    return arr;
+  }
