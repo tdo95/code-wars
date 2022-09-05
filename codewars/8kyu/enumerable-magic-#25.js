8e530da6cb26329a3b0f625cd5ad00accbcb5715
Tee O <tdopress@gmail.com>
2022-09-04 23:08:24 -0400

    new solution

---
+++ codewars/8kyu/enumerable-magic-#25.js
@@ -0,0 +1,18 @@
+/*
+Enumerable Magic #25 - Take the First N Elements
+
+https://www.codewars.com/kata/545afd0761aa4c3055001386/javascript
+
+INSTRUCTIONS
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+If you need help, here's a reference:
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+
+*/
+
+//SOLUTION
+function take(arr, n) {
+    return arr.slice(0, n);
+  }
